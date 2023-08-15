ea308330fd20f51c5540baf54b3c4db9c699bc9a
Franz <54110625+FranzZehetleitner@users.noreply.github.com>
2023-08-15 22:04:34 +0200

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,4 +33,4 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # display the normalized version
 streamlit.dataframe(fruityvice_normalized)
 
-#helloworld this is a test
+#helloworld this is a test 2 this
