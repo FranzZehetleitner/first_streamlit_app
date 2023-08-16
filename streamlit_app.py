be54ff6b5e0b4afd2c72df60259db675d50bfcdf
Franz <54110625+FranzZehetleitner@users.noreply.github.com>
2023-08-16 18:42:29 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,10 +11,8 @@ streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
-
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
-
 # Let's put a pick list here so they can pick the fruit they want to include 
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado', 'Strawberries'])
 
@@ -23,12 +21,17 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 streamlit.dataframe(fruits_to_show)
 
+
+
+
+
 def get_fruityvice_data(this_fruit_choice):
     fruityvice_response = requests.get ("https://fruityvice.com/api/fruit/" + fruit_choice)
     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
     return fruityvice_normalized
 
-  
+
+
 streamlit.header ('Fruityvice Fruit Advice')
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
@@ -39,10 +42,12 @@ try:
       streamlit.dataframe(back_from_function)
 
 except URLError as e:
+    streamlit.error()
 
 
 
-##streamlit.header("The fruit load list contains: ")
+
+streamlit.header("The fruit load list contains: ")
 #snowflake-related functions
 def get_fruit_load_list():
     with my_cnx.cursor() as my_cur
